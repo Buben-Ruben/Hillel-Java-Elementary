6b4c4c2b796557255f3918917a187282c9e4d7ec
Bubun <pavel25shevchuk@gmail.com>
2022-04-07 18:38:52 +0300

    Completed Home Work 5

---
+++ HW-5/src/Car/Car.java
@@ -0,0 +1,45 @@
+package Car;
+
+public class Car {
+    private String type;
+    private String model;
+    private String color;
+    private int speed;
+    private int fuel;
+
+    public Car() {
+    }
+    public Car(String type, String model, String color) {
+        this.type = type;
+        this.model = model;
+        this.color = color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+    public void setSpeed(int argument) {
+        if (argument > 0) {
+            speed += argument;
+        } else {
+            speed -= argument;
+        }
+    }
+
+    public String carInfoToString() {
+        return "\nCar Info [type=" + type + ", model=" + model + ", color=" + color + ", speed=" + speed + ", fuel=" + fuel +"]";
+    }
+
+    public void help() {
+        System.out.println("""
+                |\t[new]\t\t- to create new car
+                |\t[color]\t- to set car's color
+                |\t[fuel]\t- to set the amount of fuel
+                |\t[speed]\t\t- to increase/decrease car speed
+                |\t[info]\t- get car info
+                |\t[exit]\t\t- close program""");
+    }
+}
